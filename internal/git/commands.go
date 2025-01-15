cfcb09be705b6c8e0158db9beff295b541c17e06
RITANKAR SAHA <ritankar.saha786@gmail.com>
2025-01-15 19:36:51 +0530

    Internal commands file

---
+++ internal/git/commands.go
@@ -0,0 +1,13 @@
+package git
+
+import (
+	// "os/exec"
+)
+
+// RunCommand executes a shell command and returns the output or error
+// func RunCommand(command string) (string, error) {
+// 	cmd := exec.Command("sh", "-c", command) 
+// 	output, err := cmd.CombinedOutput()
+// 	return string(output), err
+// }
+
